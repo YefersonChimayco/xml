6c57ab597904d800a53b2ebd6c1537383db11f71
YefersonChimayco <chimaycohnj@gmail.com>
2025-11-28 10:10:35 -0500

    v

--- xmlv1.php
+++ xmlv1.php
@@ -1,4 +1,142 @@
 <?php
-$pe = ['DISEÑO Y PROGRAMACION WEB', 'ENFERMERIA TECNICA', 'INDUSTRIAS DE ALIMENTOS Y BEBIDAS','MECATRONICA ATUROMTRIZ','PRODUCCION AGROPECUARIA'];
+$ies = [];
+
+$udp1 = [
+    'Analisis y diseño de sistemas web',
+    'Funddamentos de programación',
+    'Introducción a la base de datos',
+    'Redes e internet',
+    'Arquitectura de computadoras',
+    'Comunicación oral',
+    'Aplicaciones en internet'
+];
+$udp2 = [
+    'Ofimática',
+    'Interpretación y producción de textos',
+    'Metodología de desarrollo de software',
+    'Programación orientada a objetos',
+    'Arquitectura de servidores web',
+    'Aplicaciones sistematizadas',
+    'Taller de base de datos'
+];
+$udp3 = [
+    'Administración de base de datos',
+    'Programación de aplicaciones web',
+    'Diseño de interfaces web',
+    'Pruebas de software',
+    'Inglés para la comunicación oral'
+];
+$udp4 = [
+    'Desarrollo de entornos web',
+    'Programación de soluciones web',
+    'Proyectos de software',
+    'Seguridad en aplicaciones web',
+    'Comprensión y redacción en inglés',
+    'Comportamiento ético'
+];
+$udp5 = [
+    'Programación de aplicaciones móviles',
+    'Marketing digital',
+    'Diseño de soluciones web',
+    'Gestión y administración de sitios web',
+    'Diagramación digital',
+    'Solución de problemas',
+    'Oportunidades de negocios'
+];
+$udp6 = [
+    'Plataforma de servicios web',
+    'Ilustración y gráfica digital',
+    'Administración de servidores web',
+    'Comercio electrónico',
+    'Plan de negocios'
+];// ------------------------------------PERIODOS.-------------------------------------------------------//
+
+$p1=[];
+$p1['nombre']= "I";
+$p1['unidades_didacticas']= $udp1;
+print_r($p1);
+
+$p2 = [];
+$p2['nombre'] = "II";
+$p2['unidades_didacticas'] = $udp2;
+print_r($p2);
+
+$p3 = [];
+$p3['nombre'] = "III";
+$p3['unidades_didacticas'] = $udp3;
+print_r($p3);
+
+$p4 = [];
+$p4['nombre'] = "IV";
+$p4['unidades_didacticas'] = $udp4;
+print_r($p4);
+
+$p5 = [];
+$p5['nombre'] = "V";
+$p5['unidades_didacticas'] = $udp5;
+print_r($p5);
+
+$p6 = [];
+$p6['nombre'] = "VI";
+$p6['unidades_didacticas'] = $udp6;
+print_r($p6);
+
+
+// --------------------------------------------------------------M O  D U L O S -----------------------------
+$m1 = array();
+$m1 ['nombre']= "ANALISIS Y DISEÑO DE SISTEMAS WEB";
+$m1 ['periodos']= [$p1, $p2];
+print_r($m1);
+
+$m2 = array();
+$m2 ['nombre']= "DESARROLLO DE APLICACIONES WEB";
+$m2 ['periodos']= [$p3, $p4];
+print_r($m2);
+
+$m3 = array();
+$m3 ['nombre']= "DISEÑO DE SERVICIOS WEB";
+$m3 ['periodos']= [$p5, $p6];
+print_r($m3);
+
+// --------------------------------------------------------------PROGRAMAS DE ESTUDIO -----------------------------
+
+$pe1 = array();
+$pe1['nombre'] = "DISEÑO Y PROGRAMACION WEB";
+$pe1['modulos']=[$m1,$m2,$m3];
+print_r($pe1);
+
+
+$pe2 = array();
+$pe3 = array();
+$pe4 = array();
+$pe5 = array();
+
+$ies ['nombre']= "IES público HUANTA";
+$ies ['programas_de_estudio']=[$pe1,$pe2, $pe3, $pe4, $pe5];
+
+
+$xml = new DOMDocument('1.0', 'UTF-8');
+$xml ->formatOutput = true;
+
+$et1= $xml->createElement('ies');
+$xml->appendChild($et1);
+$nombre_ies= $xml->createElement("nombre",$ies['nombre']);
+$programas_ies = $xml->createElement("programas_de_estudio");
+foreach ($ies['programas_de_estudio'] as $indice => $PEs ) {
+    $num_pe = $xml->createElement("pe".$indice+1); 
+    $nombre_pe = $xml->createElement("nombre",$PEs['nombre']);
+    $num_pe->appendChild($nombre_pe);
+    $programas_ies->appendChild($num_pe);
+
+
+}
+
+
+
+$archivo = "ies.xml";
+$xml->save($archivo);
+
+
+
 
 ?>
