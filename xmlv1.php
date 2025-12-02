6b8acbc4240f88d3259a577fd03e3e0cc582d849
YefersonChimayco <chimaycohnj@gmail.com>
2025-12-02 02:33:10 -0500

    para el examen

--- xmlv1.php
+++ xmlv1.php
@@ -1,6 +1,8 @@
 <?php
 $ies = [];
 
+//--------------------------------------UNIDADES DIDÁCTICAS-----------------------------------------
+
 $udp1 = [
     'Analisis y diseño de sistemas web',
     'Funddamentos de programación',
@@ -49,112 +51,93 @@ $udp6 = [
     'Administración de servidores web',
     'Comercio electrónico',
     'Plan de negocios'
-]; // ------------------------------------PERIODOS.-------------------------------------------------------//
+];
 
-$p1 = [];
-$p1['nombre'] = "I";
-$p1['unidades_didacticas'] = $udp1;
+//--------------------------------------PERIODOS-----------------------------------------
 
-$p2 = [];
-$p2['nombre'] = "II";
-$p2['unidades_didacticas'] = $udp2;
+$p1 = ['nombre' => 'I', 'unidades_didacticas' => $udp1];
+$p2 = ['nombre' => 'II', 'unidades_didacticas' => $udp2];
+$p3 = ['nombre' => 'III', 'unidades_didacticas' => $udp3];
+$p4 = ['nombre' => 'IV', 'unidades_didacticas' => $udp4];
+$p5 = ['nombre' => 'V', 'unidades_didacticas' => $udp5];
+$p6 = ['nombre' => 'VI', 'unidades_didacticas' => $udp6];
 
-$p3 = [];
-$p3['nombre'] = "III";
-$p3['unidades_didacticas'] = $udp3;
+//-------------------------MODULOS-----------------------------
 
-$p4 = [];
-$p4['nombre'] = "IV";
-$p4['unidades_didacticas'] = $udp4;
+$m1 = ['nombre' => 'ANÁLISIS Y DISEÑO DE SISTEMAS WEB', 'periodos' => [$p1, $p2]];
+$m2 = ['nombre' => 'DESARROLLO DE APLICACIONES WEB', 'periodos' => [$p3, $p4]];
+$m3 = ['nombre' => 'DISEÑO DE SERVICIOS WEB', 'periodos' => [$p5, $p6]];
 
-$p5 = [];
-$p5['nombre'] = "V";
-$p5['unidades_didacticas'] = $udp5;
+//-------------------------PROGRAMAS DE ESTUDIO-----------------------------
 
-$p6 = [];
-$p6['nombre'] = "VI";
-$p6['unidades_didacticas'] = $udp6;
+$pe1 = ['nombre' => 'DISEÑO Y PROGRAMACION WEB', 'modulos' => [$m1, $m2, $m3]];
+$pe2 = ['nombre' => 'ENFERMERIA TECNICA', 'modulos' => []];
+$pe3 = ['nombre' => 'MECATRONICA AUTOMOTRIZ', 'modulos' => []];
+$pe4 = ['nombre' => 'PRODUCCION AGROPECUARIA', 'modulos' => []];
+$pe5 = ['nombre' => 'INDUSTRIAS DE ALIMENTOS Y BEBIDAS', 'modulos' => []];
 
-
-// --------------------------------------------------------------M O  D U L O S -----------------------------
-$m1 = array();
-$m1['nombre'] = "ANALISIS Y DISEÑO DE SISTEMAS WEB";
-$m1['periodos'] = [$p1, $p2];
-
-$m2 = array();
-$m2['nombre'] = "DESARROLLO DE APLICACIONES WEB";
-$m2['periodos'] = [$p3, $p4];
-
-$m3 = array();
-$m3['nombre'] = "DISEÑO DE SERVICIOS WEB";
-$m3['periodos'] = [$p5, $p6];
-
-// --------------------------------------------------------------PROGRAMAS DE ESTUDIO -----------------------------
-
-$pe1 = array();
-$pe1['nombre'] = "DISEÑO Y PROGRAMACION WEB";
-$pe1['modulos'] = [$m1, $m2, $m3];
-
-
-$pe2 = array();
-$pe2['nombre'] = "NECANICA AUTOMOTRIZ";
-$pe2['modulos'] = [];
-
-$pe3 = array();
-$pe3['nombre'] = "ENFERMERIA TECNICA";
-$pe3['modulos'] = [];
-
-$pe4 = array();
-$pe4['nombre'] = "INDUSTRIAS DE ALIMENTOS Y BEBIDAS";
-$pe4['modulos'] = [];
-
-$pe5 = array();
-$pe5['nombre'] = "PRODUCCION AGROPECUARIA";
-$pe5['modulos'] = [];
-
-$ies['nombre'] = "IES público HUANTA";
+$ies['nombre'] = "IE Público HUANTA";
 $ies['programas_de_estudio'] = [$pe1, $pe2, $pe3, $pe4, $pe5];
 
+//----------------------------- XML ----------------------------------
 
 $xml = new DOMDocument('1.0', 'UTF-8');
 $xml->formatOutput = true;
 
-$et1 = $xml->createElement('ies');
-$xml->appendChild($et1);
-$nombre_ies = $xml->createElement("nombre", $ies['nombre']);
-$programas_ies = $xml->createElement("programas_de_estudio");
-$et1->appendChild($nombre_ies);
-$et1->appendChild($programas_ies);
-foreach ($ies['programas_de_estudio'] as $indice => $PEs) {
-    $num_pe = $xml->createElement("pe" . $indice + 1);
-    $nombre_pe = $xml->createElement("nombre", $PEs['nombre']);
+$root = $xml->createElement('institucion_educativa');
+$xml->appendChild($root);
 
-    foreach ($PEs['modulos'] as $indice_modulo => $Modulo) {
-        $num_mod = $xml->createElement("mod" . $indice_modulo + 1);
-        $nom_mod = $xml->createElement("nombre", $Modulo['nombre']);
+// Nombre de la institución
+$root->appendChild($xml->createElement('nombre', $ies['nombre']));
 
-        foreach ($Modulo['periodos'] as $indice_periodo => $Periodo) {
-            $num_per = $xml->createElement("per" . $indice_periodo + 1);
-            $nom_per = $xml->createElement("nombre", $Periodo['nombre']);
-            $uds = $xml->createElement("unidades_didacticas");
+// Contenedor principal
+$programas = $xml->createElement('programas_de_estudio');
+$root->appendChild($programas);
 
-            foreach ($Periodo['unidades_didacticas'] as $indice_ud => $Ud) {
-                $num_ud = $xml->createElement("ud" . $indice_ud + 1);
-                $nom_ud = $xml->createElement("nombre",$Ud);
-                $num_ud->appendChild($nom_ud);
-                $uds->appendChild($num_ud);
+// Recorrer los programas
+foreach ($ies['programas_de_estudio'] as $i => $pe) {
+
+    $peNode = $xml->createElement('pe' . ($i + 1));
+    $programas->appendChild($peNode);
+
+    // Nombre del programa
+    $peNode->appendChild($xml->createElement('nombre', $pe['nombre']));
+
+    // Modulos
+    foreach ($pe['modulos'] as $j => $mod) {
+
+        $modNode = $xml->createElement('modulo' . ($j + 1));
+        $peNode->appendChild($modNode);
+
+        // Nombre del módulo
+        $modNode->appendChild($xml->createElement('nombre', $mod['nombre']));
+
+        // Periodos
+        foreach ($mod['periodos'] as $k => $per) {
+
+            $perNode = $xml->createElement('periodo' . ($k + 1));
+            $modNode->appendChild($perNode);
+
+            // Nombre del periodo
+            $perNode->appendChild($xml->createElement('nombre', $per['nombre']));
+
+            // Unidades
+            $udsNode = $xml->createElement('unidades_didacticas');
+            $perNode->appendChild($udsNode);
+
+            foreach ($per['unidades_didacticas'] as $u => $ud) {
+
+                $udNode = $xml->createElement('ud');
+                $udsNode->appendChild($udNode);
+
+                $udNode->appendChild($xml->createElement('nombre', $ud));
             }
-            $num_per->appendChild($nom_per);
-            $num_per->appendChild($uds);
-            $num_mod->appendChild($num_per);
         }
-        $num_mod->appendChild($nom_mod);
-        $num_pe->appendChild($num_mod);
     }
-    $num_pe->appendChild($nombre_pe);
-    $programas_ies->appendChild($num_pe);
 }
 
+// Guardar archivo
 $archivo = "ies.xml";
 $xml->save($archivo);
-?>
+
+echo "XML generado correctamente: $archivo";
---
+++ xmlv2.php
@@ -0,0 +1,112 @@
+<?php
+$conexion = new mysqli("localhost", "root", "root", "para_xml");
+if ($conexion->connect_errno) {
+    echo "fallo la conexion a la base de dattos (" . $conexion->connect_errno . ")" . $conexion->connect_error;
+}
+$xml = new DOMDocument('1.0', 'UTF-8');
+$xml->formatOutput = true;
+
+$et1 = $xml->createElement('programas_estudio');
+$xml->appendChild($et1);
+
+
+$consulta = "SELECT * FROM sigi_programa_estudios";
+$resultado = $conexion->query($consulta);
+while ($pe = mysqli_fetch_assoc($resultado)) {
+    echo $pe['nombre'] . "<br>";
+    $num_pe = $xml->createElement('pe_' . $pe['id']);
+
+    $codigo_pe = $xml->createElement('codigo', $pe['codigo']);
+    $num_pe->appendChild($codigo_pe);
+
+    $tipo_pe = $xml->createElement('tipo', $pe['tipo']);
+    $num_pe->appendChild($tipo_pe);
+
+    $nombre_pe = $xml->createElement('nombre', $pe['nombre']);
+    $num_pe->appendChild($nombre_pe);
+    //---------------------------
+    $et_plan = $xml->createElement('planes_estudio');
+    $consulta_plan = "SELECT*FROM sigi_planes_estudio WHERE id_programa_estudios=" . $pe['id'];
+    $resultado_plan = $conexion->query($consulta_plan);
+    while ($plan = mysqli_fetch_assoc($resultado_plan)) {
+        echo $plan['nombre'] . "<br>";
+        $num_plan = $xml->createElement('plan_' . $plan['id']);
+
+        $nombre_plan = $xml->createElement('nombre', $plan['nombre']);
+        $num_plan->appendChild($nombre_plan);
+
+        $resolucion_plan = $xml->createElement('resolucion', $plan['resolucion']);
+        $num_plan->appendChild($resolucion_plan);
+
+        $fecha_plan = $xml->createElement('fecha_registro', $plan['fecha_registro']);
+        $num_plan->appendChild($fecha_plan);
+
+        //--------------------------
+        $et_plan = $xml->createElement('modulos');
+        $consulta_mod = "SELECT*FROM sigi_modulo_formativo WHERE id_plan_estudio=" . $plan['id'];
+        $resultado_mod = $conexion->query($consulta_mod);
+        while ($mod = mysqli_fetch_assoc($resultado_mod)) {
+            echo $mod['descripcion'] . "<br>";
+            $num_mod = $xml->createElement('mod_' . $mod['id']);
+
+            $nro_mod = $xml->createElement('nro_modulo', $mod['nro_modulo']);
+            $num_mod->appendChild($nro_mod);
+
+            $descripcion_mod = $xml->createElement('descripcion', $mod['descripcion']);
+            $num_mod->appendChild($descripcion_mod);
+
+            //----------------
+      $et_plan = $xml->createElement('semestres');
+        $consulta_se = "SELECT*FROM sigi_semestre WHERE id_modulo_formativo=" . $plan['id'];
+        $resultado_se = $conexion->query($consulta_se);
+        while ($se = mysqli_fetch_assoc($resultado_se)) {
+            echo $se['descripcion'] . "<br>";
+            $num_se = $xml->createElement('se_' . $se['id']);
+
+            $descripcion_se = $xml->createElement('descripcion', $se['descripcion']);
+            $num_se->appendChild($descripcion_se);
+
+//-----------
+$et_plan = $xml->createElement('unidaddes_didacticas');
+        $consulta_ud = "SELECT*FROM sigi_unidad_didactica WHERE id_semestre=" . $plan['id'];
+        $resultado_ud = $conexion->query($consulta_ud);
+        while ($ud = mysqli_fetch_assoc($resultado_ud)) {
+            echo $ud['nombre'] . "<br>";
+            $num_ud = $xml->createElement('se_' . $ud['id']);
+
+            $nombre_ud = $xml->createElement('nombre', $ud['nombre']);
+            $num_ud->appendChild($nombre_ud);
+             $creditos_teorico_ud = $xml->createElement('creditos_teorico', $ud['creditos_teorico']);
+            $num_ud->appendChild($creditos_teorico_ud);
+ $creditos_practico_ud = $xml->createElement('creditos_practico', $ud['creditos_practico']);
+            $num_ud->appendChild($creditos_practico_ud);
+
+             $tipo_ud = $xml->createElement('tipo', $ud['tipo']);
+            $num_ud->appendChild($tipo_ud);
+            
+             $orden_ud = $xml->createElement('orden', $ud['orden']);
+            $num_ud->appendChild($orden_ud);
+
+
+            $num_se->appendChild($num_ud);
+        }
+            $num_mod->appendChild($num_se);
+
+        }
+
+            $num_plan->appendChild($num_mod);
+    }
+            $et_plan->appendChild($num_plan);
+        }
+        $num_pe->appendChild($et_plan);
+        $et1->appendChild($num_pe);
+    }
+
+
+
+
+
+    $archivo = "ies2.xml";
+    $xml->save($archivo);
+
+
